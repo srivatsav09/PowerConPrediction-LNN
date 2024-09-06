541d3a8dddcd5adb3759b393a689e142d200b29e
Srivatsav09 <srivatsav.vijay2003@gmail.com>
2024-09-06 22:20:00 +0530

    deleted venv

--- .venv/Lib/site-packages/ncps/datasets/utils.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-from io import BytesIO
-from zipfile import ZipFile
-
-
-def download_and_unzip(url, extract_to="."):
-    http_response = urlopen(url)
-    zipfile = ZipFile(BytesIO(http_response.read()))
-    zipfile.extractall(path=extract_to)
